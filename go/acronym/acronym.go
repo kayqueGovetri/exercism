d1c46fe033221d50787c7be2ffaf11643781e127
kayqueGovetri <govetrikayque@gmail.com>
2026-08-30 20:06:51 -0300

    feat(go): solve acronym exercise

---
+++ go/acronym/acronym.go
@@ -0,0 +1,33 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package acronym should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package acronym
+
+import (
+	"strings"
+	"unicode"
+)
+
+// Abbreviate should have a comment documenting it.
+func Abbreviate(s string) string {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+	s = strings.ReplaceAll(s, "-", " ")
+	var acronym strings.Builder
+	newWord := true
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			if newWord {
+				acronym.WriteRune(unicode.ToUpper(r))
+				newWord = false
+			}
+		} else if unicode.IsSpace(r) {
+			newWord = true
+		}
+	}
+	return acronym.String()
+}
